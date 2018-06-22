0fd0efc4bf9e93ea859153a6180acee01c17f9f6
diego lauriano de souza <s2it_dsouza@uolinc.com>
2018-06-22 17:31:37 -0300

    adicionado o teste de remover telefone e logout

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -1,6 +1,7 @@
 package tests;
 
-import jdk.nashorn.internal.AssertsEnabled;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,10 +10,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.internal.WebElementToJsonConverter;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InformacoesUsuarioTest {
 
@@ -21,24 +21,17 @@ public class InformacoesUsuarioTest {
     @Before
     public void setUp()
     {
-        System.setProperty("webdriver.chrome.driver","/home/academia/Drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver","/home/s2it_dsouza/Drivers/chromedriver");
         navegador = new ChromeDriver();
         //abrir o site
         navegador.get("http://www.juliodelima.com.br/taskit");
         navegador.manage().window().maximize();
         navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-    }
-
-    @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario()
-    {
         //clicar em signin
-       navegador.findElement(By.linkText("Sign in")).click();
+        navegador.findElement(By.linkText("Sign in")).click();
 
         //Identificando o formulario de login
         WebElement formularioSinginBox = navegador.findElement(By.id("signinbox"));
-
 
         // digitar no campo name "login" o texto julio001
         formularioSinginBox.findElement(By.name("login")).sendKeys("julio0001");
@@ -46,16 +39,20 @@ public class InformacoesUsuarioTest {
         // clicar no campo com name "password" que esta dentro no formulario com id "signinbox"
         formularioSinginBox.findElement(By.name("password")).sendKeys("123456");
 
-
         // clicar no LINK com o texto SIGN IN
         navegador.findElement(By.linkText("SIGN IN")).click();
-
 
         // clicar em um link que possui a class 'me'
         navegador.findElement(By.className("me")).click();
 
         // clicar em um link que possui o texto
         navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
+
+    }
+
+   //  @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario()
+    {
 
         // clicar em um botao atraves do seu xpath
         navegador.findElement(By.xpath("//button[@data-target='addmoredata']")).click();
@@ -86,6 +83,31 @@ public class InformacoesUsuarioTest {
 
         // Assert.assertEquals("Hi, Julio", textoNoElementoMe);
 
+
+
+    }
+
+    @Test
+    public void removerUmContatoDeUmUsuario()
+    {
+        // encontrar e clicar n o meu telefone pelo xpath: //span[text()="+5516912345678"]/following-sibling::a
+        navegador.findElement(By.xpath("//span[text()='+5516912345678']/following-sibling::a")).click();
+
+        // interagir com o pop up java scrip
+        navegador.switchTo().alert().accept();
+
+        // validar a exibiçao da 'Rest in peace, dear phone!'
+        WebElement mensagemSucess = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemSucess.getText();
+        Assert.assertEquals("Rest in peace, dear phone!", mensagem);
+
+        // aguardar ate 10 seg para que a janela desapareça
+        WebDriverWait aguardar = new WebDriverWait(navegador,10);
+        aguardar.until(ExpectedConditions.stalenessOf(mensagemSucess));
+
+        // fazer logout, clicando no link com o texto logout
+        navegador.findElement(By.linkText("Logout")).click();
+        
 
 
     }
